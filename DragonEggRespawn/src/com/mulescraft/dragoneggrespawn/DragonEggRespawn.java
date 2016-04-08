9314b919d9c1623cea6dfc6a40a3374808a1a4e0
R-Mule <asmit119@me.com>
2016-04-08 16:37:23 -0400

    Release Version 1.0
    Well that was easy.

--- DragonEggRespawn/src/com/mulescraft/dragoneggrespawn/DragonEggRespawn.java
+++ DragonEggRespawn/src/com/mulescraft/dragoneggrespawn/DragonEggRespawn.java
@@ -1,7 +1,10 @@
 package com.mulescraft.dragoneggrespawn;
 
 import org.bukkit.ChatColor;
+import org.bukkit.Material;
 import org.bukkit.Server;
+import org.bukkit.World;
+import org.bukkit.block.Block;
 import org.bukkit.command.ConsoleCommandSender;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.LivingEntity;
@@ -10,16 +13,17 @@ import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDeathEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
+
 public class DragonEggRespawn extends JavaPlugin implements Listener{
 	Server server = this.getServer();
 	ConsoleCommandSender console = server.getConsoleSender();
-	
+
 	//locations for dragon egg.
 	public int x;
 	public int y;
 	public int z;
-	
-	
+
+
 	@Override
 	public void onEnable(){
 		Documentation doc = new Documentation();
@@ -28,32 +32,38 @@ public class DragonEggRespawn extends JavaPlugin implements Listener{
 		console.sendMessage(ChatColor.BLUE+"[DragonEggRespawn] Config Loaded!");
 		server.getPluginManager().registerEvents(this, this);
 	}//end onEnable()
-	
-	
+
+
 	@Override
 	public void onDisable(){
 		console.sendMessage(ChatColor.BLUE+"[DragonEggRespawn] Disabled Successfully! *High Five*");
 	}//end onDisable()
-	
-	
+
+
 	@EventHandler
 	public void onEntityDeath(EntityDeathEvent event) {
-	     LivingEntity entity = event.getEntity();
-
-	    // getType() is inherited from Entity
-	    if(entity.getType() == EntityType.ENDER_DRAGON){
-	        // LivingEntity has a getKiller() method
-	         entity.getKiller().sendMessage("You killed the dragon you bastard!");
-	         // getKiller() returns a Player, so you don't have to check if it's a player.
-	    }//end if it was a dragon
+		LivingEntity entity = event.getEntity();
+		// getType() is inherited from Entity
+		if(entity.getType() == EntityType.ENDER_DRAGON){
+			int x = getConfig().getInt("Location.X");
+			int y = getConfig().getInt("Location.Y");
+			int z = getConfig().getInt("Location.Z");
+			if(x!=-1&&y!=-1&&z!=-1){
+				World world = server.getWorld(getConfig().getString("World"));
+				Block block = world.getBlockAt(x,y,z);
+				block.setType(Material.DRAGON_EGG);//And it is done.
+			}
+		}//end if it was a dragon
 	}//end onEntityDeath()
-	
+
 	private void setupConfig(){
 		//vars used for configuration
 		getConfig().addDefault("Location.X", -1);
 		getConfig().addDefault("Location.Y", -1);
-		getConfig().addDefault("Location.Z",-1);
+		getConfig().addDefault("Location.Z", -1);
 		getConfig().addDefault("World", "world_the_end");
+		getConfig().options().copyDefaults(true);
+		saveConfig();
 	}//end setupConfig()
-	
+
 }//end class DragonEggRespawn
